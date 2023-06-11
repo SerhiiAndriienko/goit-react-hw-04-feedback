ef6cd9eb14c15a82c79783f1280cf60bbc873703
serhiiandriienko <116962959+SerhiiAndriienko@users.noreply.github.com>
2023-06-11 11:29:52 +0200

    2  commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,107 +1,50 @@
-import React, { useState, useEffect } from 'react';
-import Statistics from './feedback/statistics/statistics';
-import Options from './feedback/options/options';
-import Section from './feedback/Section/section';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Filter from './filter/Filter';
+import ContactForm from './contactForm/ContactForm';
+import ContactList from './contactList/ContactList';
 
-export function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const [total, setTotal] = useState(0);
-  const [percentOfGood, setPercentOfGood] = useState('');
-
-  const clickOnGoodBtn = () => {
-    setGood(prevState => prevState + 1);
-  };
-  const clickOnNeutralBtn = () => {
-    setNeutral(prevState => prevState + 1);
-  };
-  const clickOnBadBtn = () => {
-    setBad(prevState => prevState + 1);
-  };
+export default function App() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
-    const total = bad + good + neutral;
-    setTotal(total);
-  }, [good, bad, neutral]);
+    const getFromStorage = JSON.parse(localStorage.getItem('contacts'));
+    if (getFromStorage) {
+      setContacts(getFromStorage);
+    }
+  }, []);
 
-  useEffect(() => {
-    const percents = ((good * 100) / (bad + good + neutral)).toFixed(0);
-    setPercentOfGood(percents);
-  }, [good, bad, neutral]);
+  const filterChange = filterValue => {
+    setFilter(filterValue);
+  };
 
+  const handleValueChange = array => {
+    setContacts(array);
+  };
+
+  const onDeleteContact = contactId => {
+    const newArray = contacts.filter(contact => contact.id !== contactId);
+    handleValueChange(newArray);
+    if (newArray.length === 0) {
+      localStorage.removeItem('contacts');
+    }
+  };
   return (
     <div>
-      <Section title="Please leave feedback">
-        <Options
-          clickOnButtonG={clickOnGoodBtn}
-          clickOnButtonN={clickOnNeutralBtn}
-          clickOnButtonB={clickOnBadBtn}
-        ></Options>
-        <h2>Statistics</h2>
-        <Statistics
-          good={good}
-          neutral={neutral}
-          bad={bad}
-          total={total}
-          positivePercentage={percentOfGood + '%'}
-        ></Statistics>
-      </Section>
+      <h1>Phonebook</h1>
+      <ContactForm
+        contacts={contacts}
+        handleValueChange={handleValueChange}
+      ></ContactForm>
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} filterChange={filterChange}></Filter>
+      <ContactList
+        filter={filter}
+        contacts={contacts}
+        onDeleteContact={onDeleteContact}
+      ></ContactList>
     </div>
   );
 }
-
-// export class App extends Component {
-//   state = {
-//     good: 0,
-//     neutral: 0,
-//     bad: 0,
-//   };
-//   goode = () => {
-//     this.setState(prevState => ({
-//       good: (prevState.good += 1),
-//     }));
-//   };
-//   neutraly = () => {
-//     this.setState(prevState => ({
-//       neutral: (prevState.neutral += 1),
-//     }));
-//   };
-//   bady = () => {
-//     this.setState(prevState => ({
-//       bad: (prevState.bad += 1),
-//     }));
-//   };
-//   countTotalFeedback = () => {
-//     return this.state.bad + this.state.good + this.state.neutral;
-//   };
-//   countPositiveFeedbackPercentage = () => {
-//     return `${(
-//       (this.state.good * 100) /
-//       (this.state.bad + this.state.good + this.state.neutral)
-//     ).toFixed(0)}%`;
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <Section title="Please leave feedback">
-//           <Options
-//             clickOnButtonG={this.goode}
-//             clickOnButtonN={this.neutraly}
-//             clickOnButtonB={this.bady}
-//           ></Options>
-//           <h2>Statistics</h2>
-//           <Statistics
-//             good={this.state.good}
-//             neutral={this.state.neutral}
-//             bad={this.state.bad}
-//             total={this.countTotalFeedback}
-//             positivePercentage={this.countPositiveFeedbackPercentage}
-//           ></Statistics>
-//         </Section>
-//       </div>
-//     );
-//   }
-// }
--- src/components/feedback/notification/notification.js
+++
@@ -1,5 +0,0 @@
-function Notification() {
-  return <p>There is no feedback</p>;
-}
-
-export default Notification;
